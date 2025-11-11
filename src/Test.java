9b589a841bc28d839ad82cf1dd2246cd17843169
alinmihalache <alin.g.mhll@gmail.com>
2025-11-11 21:20:36 +0200

    Delete

--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        GUI1 ecran = new GUI1("Fereastra");
-        ecran.setSize(500, 100);
-        ecran.setVisible(true);
-    }
-}
