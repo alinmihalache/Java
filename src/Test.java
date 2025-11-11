efee97648873ff428bbe1de9ba3c6d08f1d253e5
alinmihalache <alin.g.mhll@gmail.com>
2025-11-11 19:55:08 +0200

    Added working AWT GUI example (Frame + Button)

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        GUI1 ecran = new GUI1("Fereastra");
+        ecran.setSize(500, 100);
+        ecran.setVisible(true);
+    }
+}
